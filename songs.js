c9d0f5a91482e67d75b12a75cf750c61f204d2b1
vlad mamotenko <vladuk540@gmail.com>
2024-12-26 03:18:28 +0200

    add menu after click

--- songs.js
+++ songs.js
@@ -5,22 +5,24 @@ const menuSongEl = document.querySelector(".main-navbar")
 const mouseOverEl = document.querySelectorAll(".navbar-menu")
 const mouseOver = document.querySelector(".mousemove")
 menuSongEl.addEventListener("click", () => {
-    mouseOver.volume = 1
+    mouseOver.volume = 0.6
     menuSwitch.play()
     console.log("click")
 })
 
 mouseOverEl[0].addEventListener("mouseover", () => {
     mouseOver.play()
-    mouseOver.volume = 0.5
+    mouseOver.volume = 0.4
 })
 mouseOverEl[1].addEventListener("mouseover", () => {
     mouseOver.play()
-    mouseOver.volume = 0.5
+    mouseOver.volume = 0.4
 })
 mouseOverEl[2].addEventListener("mouseover", () => {
     mouseOver.play()
-    mouseOver.volume = 0.5
+    mouseOver.volume = 0.4
 })
 
 //// controls songs
+/// bg-music
+// document.querySelector(".bg-music").volume = 0.2
