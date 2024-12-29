e9d594a9a6308d8d32a3af6c9ddbd869e46c5aaf
Yevhen <zubloevgene@gmail.com>
2024-12-29 16:29:50 +0200

    adding player hp reduction

--- level_3/level3playerXp.js
+++ level_3/level3playerXp.js
@@ -1,7 +1,27 @@
 "use strict"
 
-const plaerXP = document.getElementsByClassName("playerXP")
 
-function xp () {
-   
-}
+// const player = {
+//    hp: 100,
+//    hpObserver:new Observer (),
+//    upDateHp(value){
+//     this.hp = value;
+//     hpObserver.broadCast();
+//    }
+
+
+// }
+
+const bock = document.getElementById('xpPlay')
+let currentWidth = 300;
+
+const interval = setInterval(() => {
+    currentWidth *= 0.9;
+
+    if (currentWidth <= 0){
+        clearInterval(interval);
+        bock.style.display ='none'
+    }
+    bock.style.width = `${currentWidth}px`;
+},2000);
+
