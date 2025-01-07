1a3132eef5d98125f0fba85509bcebbc950c95f3
Yevhen <zubloevgene@gmail.com>
2025-01-07 21:26:44 +0200

    Добавления уменьшения жизни врагов

--- level_3/level3Opponents.js
+++ level_3/level3Opponents.js
@@ -1,19 +1,32 @@
 "use strict"
 
-function hpEnemy (){
 
-   const decrease = document.getElementById('decrease')
+// Уменьшения блока жизни врага до 0
+const boss = document.getElementById('boss')
    let width = 300;
-   decrease.addEventListener('click',() =>{
-    const currentWidth = parseInt(getComputedStyle(decrease).width)
-
-    if(width > 20){
-        width -=10
+   const decrease = document.getElementById('decrease')
+   boss.addEventListener('click',() =>{
+    const currentWidth = parseInt(window.getComputedStyle(decrease).width)
+    console.log("click")
+    if(width >= 0){
+        width -=30
         decrease.style.width = width + 'px'
+    }else if(width=== 0){
+        console.log('game over')
     }
    })
-    
-    
-}
+//Уменьшения текста жизни у врага от 100  до 0
 
-hpEnemy()
+   const bossProsent = document.getElementById('bossProsent')
+
+   let correntProsent = parseInt(bossProsent.textContent);
+
+   boss.addEventListener('click',() =>{
+
+    
+    correntProsent -= 10;
+    bossProsent.textContent = correntProsent;
+    if(correntProsent === 0){
+        bossProsent.textContent = 0
+    }
+   })
